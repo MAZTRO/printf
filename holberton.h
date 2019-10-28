2edfa1e0eb875b4444f893473126352e01c14237
MAZTRO <designe.mallet@gmail.com>
2019-10-28 16:33:25 -0500

    Headers for others files

---
+++ holberton.h
@@ -0,0 +1,16 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <unistd.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+int percent(const char *module);
+void case_c(char our_char);
+int case_s(void *arguments);
+int _our_write(char c);
+int _printf(const char *format, ...);
+int switch_task_0(const char *module, int count, void *arguments);
+
+#endif
